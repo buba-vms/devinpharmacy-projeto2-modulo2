0e16c84a1c4b4b62aaff389613231fe9379a75f2
buba-vms <victor.buba@live.com>
2022-12-16 23:53:01 -0300

    repositórios farmacia e endereco adicionados

---
+++ src/main/java/com/pharmacy/devin/repository/FarmaciaRepository.java
@@ -0,0 +1,7 @@
+package com.pharmacy.devin.repository;
+
+import com.pharmacy.devin.entity.Farmacia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FarmaciaRepository extends JpaRepository<Farmacia, Long> {
+}
