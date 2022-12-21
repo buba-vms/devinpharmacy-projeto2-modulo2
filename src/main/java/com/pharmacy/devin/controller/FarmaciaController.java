1498fbf1440715650b30de7400a5a1c9fafaedd7
buba-vms <victor.buba@live.com>
2022-12-21 14:21:15 -0300

    adicionado cross origin para poder fazer requisição da api  em react

--- src/main/java/com/pharmacy/devin/controller/FarmaciaController.java
+++ src/main/java/com/pharmacy/devin/controller/FarmaciaController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("farmacias")
+@RequestMapping(value = "farmacias")
+@CrossOrigin(origins = "*")
 public class FarmaciaController {
 
     private final FarmaciaService farmaciaService;
