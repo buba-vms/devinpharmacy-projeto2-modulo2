568faec019c97a62201a969abdeac48123aeced4
buba-vms <victor.buba@live.com>
2023-01-15 10:18:24 -0300

    README Adicionado

--- src/main/java/com/pharmacy/devin/controller/MedicamentoController.java
+++ src/main/java/com/pharmacy/devin/controller/MedicamentoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("medicamentos")
+@CrossOrigin(origins = "*")
 public class MedicamentoController {
 
     private MedicamentoService medicamentoService;
